9d1f69a7e7c6a8024b1ac945dcd83c85d410f1f7
pparouty <pparouty@ensc.fr>
2025-04-03 12:35:39 +0200

    tentative de fix pb front sur ubuntu

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8afdc767b43257a8cb7f168acbf37493bb446d5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+286bd1652179d8c87be135341f0b7a626026fc17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
